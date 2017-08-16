7cb3b57ff45ed64576f10659fd144c24cadecbb4
evgenyf <evgeny123456>
2017-08-16 15:21:05 +0300

    added path to directory with all data csv files

--- bin/run-load.sh
+++ bin/run-load.sh
@@ -1,2 +1,3 @@
 I9E_HOME=/home/evgeny/dev/builds/IE/gigaspaces-insightedge-2.1.0-ga-100-premium
-java -cp ../target/classes:$I9E_HOME/lib/insightedge-core.jar:$I9E_HOME/jars/*:$I9E_HOME/datagrid/lib/required/*:$I9E_HOME/datagrid/lib/optional/spatial/* nyctaxi.LoadData /home/evgeny/Downloads/csvs/yellow_tripdata_2016-01.csv,/home/evgeny/Downloads/csvs/yellow_tripdata_2016-02.csv
+DATA_FILES_HOME=/home/evgeny/Downloads/csvs
+java -cp ../target/classes:$I9E_HOME/lib/insightedge-core.jar:$I9E_HOME/jars/*:$I9E_HOME/datagrid/lib/required/*:$I9E_HOME/datagrid/lib/optional/spatial/* nyctaxi.LoadData $DATA_FILES_HOME/yellow_tripdata_2016-01.csv,$DATA_FILES_HOME/yellow_tripdata_2016-02.csv
